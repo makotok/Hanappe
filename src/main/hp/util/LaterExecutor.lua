ea9dd56b415b7e398c43cd0e1d693e3bf1714e37
makoto <makotok1123@gmail.com>
2012-06-25 02:55:27 +0900

    Additional utility of a deferred call.

---
+++ src/main/hp/util/LaterExecutor.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+-- This is a utility class to do a comparison of the object.<br>
+-- @class table
+-- @name LaterExecutor
+--------------------------------------------------------------------------------
+local M = {}
+
+--------------------------------------------------------------------------------
+-- 指定した関数を遅延実行します.<br>
+-- @param func 対象の関数.
+-- @param ... 関数に渡す引数.
+--------------------------------------------------------------------------------
+function M.callLater(func, ...)
+    local thread = MOAICoroutine.new()
+    local args = {...}
+    thread:run(
+        function()
+            func(unpack(args))
+        end
+    )
+end
+
+
+
+return M
