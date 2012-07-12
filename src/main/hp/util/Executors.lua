4ce92b36c7c907a9ea9aaad6004d1f81df7ed9b7
makoto <makotok1123@gmail.com>
2012-07-12 22:34:56 +0900

    Add a function for deferred execution.

--- src/main/hp/util/Executors.lua
+++ src/main/hp/util/Executors.lua
@@ -6,18 +6,47 @@
 local M = {}
 
 --------------------------------------------------------------------------------
--- Run the specified function delay.<br>
+-- Run the specified function delay. <br>
 -- @param func Target function.
 -- @param ... Argument.
 --------------------------------------------------------------------------------
 function M.callLater(func, ...)
+    M.callLaterFrame(0, func, ...)
+end
+
+--------------------------------------------------------------------------------
+-- Run the specified function delay. <br>
+-- @param frame Delay frame count.
+-- @param func Target function.
+-- @param ... Argument.
+--------------------------------------------------------------------------------
+function M.callLaterFrame(frame, func, ...)
     local thread = MOAICoroutine.new()
     local args = {...}
+    local count = 0
     thread:run(
         function()
+            while count < frame do
+                count = count + 1
+                coroutine.yield()
+            end
             func(unpack(args))
         end
     )
+end
+
+--------------------------------------------------------------------------------
+-- Run the specified function delay. <br>
+-- @param time Delay seconds.
+-- @param func Target function.
+-- @param ... Argument.
+--------------------------------------------------------------------------------
+function M.callLaterTime(time, func, ...)
+    local args = {...}
+    local timer = MOAITimer.new()
+    timer:setSpan(time)
+    timer:setListener(MOAITimer.EVENT_STOP, function() func(unpack(args)) end)
+    timer:start()
 end
 
 return M
