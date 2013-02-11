ffb5c76f771822c12a578997cfac1a84d69f9a8d
makoto <makotok1123@gmail.com>
2013-02-11 21:34:44 +0900

    Modification of the sample

--- projects/flower-samples/src/main_scene.lua
+++ projects/flower-samples/src/main_scene.lua
@@ -4,24 +4,14 @@ module(..., package.seeall)
 -- Constraints
 --------------------------------------------------------------------------------
 
-SAMPLES = {
-    {title = "Image",       scene = "samples/image_sample",         openAnime = "fade",         closeAnime = "fade"},
-    {title = "SheetImage",  scene = "samples/sheetimage_sample",    openAnime = "crossFade",    closeAnime = "crossFade"},
-    {title = "MovieClip",   scene = "samples/movieclip_sample",     openAnime = "popIn",        closeAnime = "popOut"},
-    {title = "MapImage",    scene = "samples/mapimage_sample",      openAnime = "slideLeft",    closeAnime = "slideRight"},
-    {title = "NineImage",   scene = "samples/nineimage_sample",     openAnime = "slideLeft",    closeAnime = "slideRight"},
-    {title = "Label",       scene = "samples/label_sample",         openAnime = "slideRight",   closeAnime = "slideLeft"},
-    {title = "Group",       scene = "samples/group_sample",         openAnime = "slideRight",   closeAnime = "slideLeft"},
-    {title = "Scene",       scene = "samples/scene_sample",         openAnime = "slideTop",     closeAnime = "slideBottom"},
-    {title = "Touch",       scene = "samples/touch_sample",         openAnime = "slideBottom",  closeAnime = "slideTop"},
-    {title = "Input",       scene = "samples/input_sample",         openAnime = "change",       closeAnime = "change"},
-}
+SAMPLES = require "samples/scene_list"
 
 --------------------------------------------------------------------------------
 -- Variables
 --------------------------------------------------------------------------------
 
 local selectedData = nil
+local backButton = nil
 
 --------------------------------------------------------------------------------
 -- Functions
@@ -38,11 +28,60 @@ function createBackButton(childScene)
     
     local label = flower.Label("Back", 100, 30)
     
-    local backButton = flower.Group(layer)
+    backButton = flower.Group(layer)
     backButton:setPos(flower.viewWidth - 100, 0)
     backButton:addChild(rect)
     backButton:addChild(label)
     backButton:addEventListener("touchDown", backButton_onTouchDown)
+end
+
+function createMenuList()
+    menuList = {}
+    local itemWidth = flower.viewWidth - 20
+    local itemHeight = 30
+    
+    for i, item in ipairs(SAMPLES) do
+        local menuItem = createMenuItem(item, itemWidth, itemHeight)
+        menuItem:setPos(10, i * 40)
+        table.insert(menuList, menuItem)
+    end
+end
+
+function createMenuItem(item, itemWidth, itemHeight)
+    local rect = flower.Rect(itemWidth, itemHeight)
+    rect:setColor(0, 0, 0.5, 1)
+
+    local label = flower.Label(item.title, itemWidth, itemHeight)
+
+    local menuItem = flower.Group(layer, itemWidth, itemHeight)
+    menuItem.data = item
+    menuItem:addChild(rect)
+    menuItem:addChild(label)
+    menuItem:addEventListener("touchDown", menuItem_onTouchDown)
+    
+    function menuItem:setSize(width, height)
+        flower.Group.setSize(self, width, height)
+        for i, child in ipairs(self.children) do
+            child:setSize(width, height)
+        end
+    end
+    
+    return menuItem    
+end
+
+function resizeMenuList()
+    local itemWidth = flower.viewWidth - 20
+    local itemHeight = 30
+    
+    for i, menuItem in ipairs(menuList) do
+        menuItem:setSize(itemWidth, itemHeight)
+    end
+end
+
+function resetPosition()
+    if backButton then
+       backButton:setPos(flower.viewWidth - 100, 0)
+    end
 end
 
 --------------------------------------------------------------------------------
@@ -50,35 +89,21 @@ end
 --------------------------------------------------------------------------------
 
 function onCreate(e)
-    -- layer
     layer = flower.Layer()
     layer:setTouchEnabled(true)
     scene:addChild(layer)
     
-    -- samples
-    local itemWidth = flower.viewWidth - 20
-    for i, item in ipairs(SAMPLES) do
-        local rect = flower.Rect(itemWidth, 30)
-        rect:setColor(0, 0, 0.5, 1)
-    
-        local label = flower.Label(item.title, itemWidth, 30)
-
-        local menuitem = flower.Group(layer)
-        menuitem.data = item
-        menuitem:setPos(10, (i * 40))
-        menuitem:addChild(rect)
-        menuitem:addChild(label)
-        menuitem:addEventListener("touchDown", menuitem_onTouchDown)
-    end
+    createMenuList()
     
     flower.Runtime:addEventListener("resize", onResize)
 end
 
 function onResize(e)
-    print("width = " .. e.width, "height = " .. e.height)
+    resizeMenuList()
+    resetPosition()
 end
 
-function menuitem_onTouchDown(e)
+function menuItem_onTouchDown(e)
     if flower.SceneMgr.transitioning then
         return
     end
@@ -98,6 +123,8 @@ function backButton_onTouchDown(e)
     if flower.SceneMgr.transitioning then
         return
     end
-
+    
     flower.closeScene({animation = selectedData.closeAnime})
+    selectedData = nil
+    backButton = nil
 end
---
+++ projects/flower-samples/src/samples/scene_list.lua
@@ -0,0 +1,12 @@
+return {
+    {title = "Image",       scene = "samples/image_sample",         openAnime = "fade",         closeAnime = "fade"},
+    {title = "SheetImage",  scene = "samples/sheetimage_sample",    openAnime = "crossFade",    closeAnime = "crossFade"},
+    {title = "MovieClip",   scene = "samples/movieclip_sample",     openAnime = "popIn",        closeAnime = "popOut"},
+    {title = "MapImage",    scene = "samples/mapimage_sample",      openAnime = "slideLeft",    closeAnime = "slideRight"},
+    {title = "NineImage",   scene = "samples/nineimage_sample",     openAnime = "slideLeft",    closeAnime = "slideRight"},
+    {title = "Label",       scene = "samples/label_sample",         openAnime = "slideRight",   closeAnime = "slideLeft"},
+    {title = "Group",       scene = "samples/group_sample",         openAnime = "slideRight",   closeAnime = "slideLeft"},
+    {title = "Scene",       scene = "samples/scene_sample",         openAnime = "slideTop",     closeAnime = "slideBottom"},
+    {title = "Touch",       scene = "samples/touch_sample",         openAnime = "slideBottom",  closeAnime = "slideTop"},
+    {title = "Input",       scene = "samples/input_sample",         openAnime = "change",       closeAnime = "change"},
+}
