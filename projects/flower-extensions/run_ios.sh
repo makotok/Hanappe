3c1769e5c9c390c77e029689f706abcee6ab3f17
makoto <makotok1123@gmail.com>
2013-02-24 22:13:47 +0900

    #31 Improvement to build and run.

---
+++ projects/flower-extensions/run_ios.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+cd `dirname $0`
+
+# moai setting
+MOAI_HOME=~/moai-sdk
+MOAI_IOS="$MOAI_HOME/hosts/xcode/ios"
+MOAI_TARGET="moai-target"
+MOAI_LUA_MODULES="$MOAI_HOME/include/lua-modules"
+
+# ios setting
+IOS_BUILD_TARGET="MoaiSample"
+IOS_BUILD_APP="$IOS_BUILD_TARGET.app"
+IOS_BUILD_CONFIG="Release"
+IOS_BUILD_JOB="flower"
+IOS_BUILD_SDK="iphonesimulator"
+IOS_BUILD_DIR="/tmp/$IOS_BUILD_JOB/ios/$IOS_BUILD_TARGET/moai/$IOS_BUILD_SDK/$IOS_BUILD_CONFIG"
+IOS_SIMULATOR_DIR=~/"Library/Application Support/iPhone Simulator/6.1/Applications"
+IPHONE_SIMULATOR="iPhone Simulator.app"
+
+# local setting
+LOCAL_BIN_DIR="`pwd`/bin"
+
+# flower build
+ant build
+STATUS=$?
+if [ $STATUS != "0" ]; then
+    echo "Build Error!"
+    exit 1
+fi
+
+# ios build
+cd "$MOAI_IOS"
+echo "$LOCAL_BIN_DIR" > $MOAI_TARGET
+echo "$MOAI_LUA_MODULES" >> $MOAI_TARGET
+. package.sh
+
+xcodebuild -configuration $IOS_BUILD_CONFIG -workspace moai.xcodeproj/project.xcworkspace -scheme $IOS_BUILD_TARGET -sdk "$IOS_BUILD_SDK" build CONFIGURATION_BUILD_DIR="$IOS_BUILD_DIR"
+
+# ios install
+cd "$IOS_SIMULATOR_DIR"
+STATUS=$?
+if [ $STATUS != "0" ]; then
+    echo "iOS Install Error!"
+    exit 2
+fi
+
+
+if [ -e $IOS_BUILD_TARGET ]; then
+    rm -fr "$IOS_BUILD_TARGET"
+fi
+
+mkdir "$IOS_BUILD_TARGET"
+cd "$IOS_BUILD_TARGET"
+cp -r "$IOS_BUILD_DIR/$IOS_BUILD_APP" .
+
+# ios run
+open -a "iPhone Simulator.app"
+
