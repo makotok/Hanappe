e34742c9e3b918802c814a1b8bef0d35ca1772ae
makoto <makotok1123@gmail.com>
2013-02-24 23:25:29 +0900

    Updating one of the sample.

--- projects/hanappe-samples/src/config.lua
+++ projects/hanappe-samples/src/config.lua
@@ -8,11 +8,17 @@ MOAISim.setBoostThreshold ( 0 )
 -- Text Label
 TextLabel.DEFAULT_COLOR = {0, 0, 0, 1}
 
+-- Screen size setting
+local screenWidth = MOAIEnvironment.horizontalResolution or 320
+local screenHeight = MOAIEnvironment.verticalResolution or 480
+local viewScale = screenWidth >= 640 and 2 or 1
+
 -- Application config
 local config = {
     title = "Hanappe samples",
-    screenWidth = MOAIEnvironment.horizontalResolution or 320,
-    screenHeight = MOAIEnvironment.verticalResolution or 480,
+    screenWidth = screenWidth,
+    screenHeight = screenHeight,
+    viewScale = viewScale,
     mainScene = "samples/sample_scene",
 }
 
