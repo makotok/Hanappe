a8483a704956784d60b590b8ec3f6e4279339e46
makoto <makotok1123@gmail.com>
2013-03-20 00:38:44 +0900

    #44 improvement of flower.openWindow

--- projects/flower-extensions/samples/main.lua
+++ projects/flower-extensions/samples/main.lua
@@ -3,11 +3,15 @@ flower = require "flower"
 tiled  = require "tiled"
 gui    = require "gui"
 
--- Resource Setting
+-- setting
 flower.Resources.addResourceDirectory("assets")
+local screenWidth = MOAIEnvironment.horizontalResolution or 320
+local screenHeight = MOAIEnvironment.verticalResolution or 480
+local screenDpi = MOAIEnvironment.screenDpi or 120
+local viewScale = math.floor(screenDpi / 240) + 1
 
 -- open window
-flower.openWindow("Flower extensions", 320, 480)
+flower.openWindow("Flower extensions", screenWidth, screenHeight, viewScale)
 
 -- open scene
 flower.openScene("main_scene")
