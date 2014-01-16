63b0243346e4f07da983979c94e783383c7fb3b1
Galdred <axboureau@gmail.com>
2014-01-16 17:25:23 +0800

    Added events for middle and right mouse button press and drag

--- projects/flower-samples/src/samples/input_sample.lua
+++ projects/flower-samples/src/samples/input_sample.lua
@@ -14,6 +14,10 @@ function onOpen(e)
     flower.InputMgr:addEventListener("touchUp", onTouch)
     flower.InputMgr:addEventListener("touchMove", onTouch)
     flower.InputMgr:addEventListener("touchCancel", onTouch)
+    flower.InputMgr:addEventListener("rightClick", onMouseButton)
+    flower.InputMgr:addEventListener("middleClick", onMouseButton)
+    flower.InputMgr:addEventListener("rightMove", onMouseMove)
+    flower.InputMgr:addEventListener("middleMove", onMouseMove)
 end
 
 function onClose(e)
@@ -23,6 +27,10 @@ function onClose(e)
     flower.InputMgr:removeEventListener("touchUp", onTouch)
     flower.InputMgr:removeEventListener("touchMove", onTouch)
     flower.InputMgr:removeEventListener("touchCancel", onTouch)
+    flower.InputMgr:removeEventListener("rightclick", onMouseButton)
+    flower.InputMgr:removeEventListener("middleclick", onMouseButton)
+    flower.InputMgr:removeEventListener("rightMove", onMouseMove)
+    flower.InputMgr:removeEventListener("middleMove", onMouseMove)
 end
 
 function onKeyInput(e)
@@ -37,6 +45,20 @@ function onTouch(e)
     print("type     = " .. e.type)
     print("idx      = " .. e.idx)
     print("tapCount = " .. e.tapCount)
+    print("x        = " .. e.x)
+    print("y        = " .. e.y)
+end
+
+function onMouseButton(e)
+    print("----- onMouseButton -----")
+    print("type  = " .. e.type)
+    print("down  = " .. tostring(e.down))
+end
+
+function onMouseMove(e)
+    print("----- onMouseMove -----")
+    print("type  = " .. e.type)
+    print("down  = " .. tostring(e.down))
     print("x        = " .. e.x)
     print("y        = " .. e.y)
 end
