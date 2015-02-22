b90d43d1aa0d8cb9f60e2fb683e336f9c094f2d0
makoto <makotok1123@gmail.com>
2015-02-23 01:53:42 +0900

    Update.

--- projects/flower-extentions/flower-spine/src/flower/spine.lua
+++ projects/flower-extentions/flower-spine/src/flower/spine.lua
@@ -12,14 +12,49 @@
 
 local spine = {}
 
+----------------------------------------------------------------------------------------------------
 -- Classes
+-- @section Classes
+----------------------------------------------------------------------------------------------------
+
+---
+-- Bone Class.
+-- @see flower.spine.Bone
 spine.Bone = require "flower.spine.Bone"
+
+---
+-- Slot Class.
+-- @see flower.spine.Slot
 spine.Slot = require "flower.spine.Slot"
+
+---
+-- AtlasMgr Class.
+-- @see flower.spine.AtlasMgr
 spine.AtlasMgr = require "flower.spine.AtlasMgr"
+
+---
+-- Attachment Class.
+-- @see flower.spine.Attachment
 spine.Attachment = require "flower.spine.Attachment"
+
+---
+-- Skeleton Class.
+-- @see flower.spine.Skeleton
 spine.Skeleton = require "flower.spine.Skeleton"
+
+---
+-- Animation Class.
+-- @see flower.spine.Animation
 spine.Animation = require "flower.spine.Animation"
+
+---
+-- SpineEvent Class.
+-- @see flower.spine.SpineEvent
 spine.SpineEvent = require "flower.spine.SpineEvent"
+
+---
+-- SpineUtils Class.
+-- @see flower.spine.SpineUtils
 spine.SpineUtils = require "flower.spine.SpineUtils"
 
 return spine
